67ecae878fb28f447ada8787da512e63c9d8ba7d
shahriyorbek2007 <142685716+shahriyorbek2007@users.noreply.github.com>
2023-09-05 16:24:38 +0300

    Add files via upload

---
+++ js/mode.js
@@ -0,0 +1,25 @@
+const body = document.querySelector("body");
+const darkBtn = document.getElementById("dark-btn");
+const lightBtn = document.getElementById("light-btn");
+
+// & toggle mode
+const modeLocal = localStorage.getItem("mode");
+
+if (modeLocal) {
+  body.classList.add("darl-mode");
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+}
+const toggleModeBtn = () => {
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+  body.classList.toggle("dark-mode");
+};
+darkBtn.addEventListener("click", () => {
+  toggleModeBtn();
+  localStorage.setItem("mode", "dark-mode");
+});
+lightBtn.addEventListener("click", () => {
+  toggleModeBtn();
+  localStorage.setItem("mode", "");
+});
